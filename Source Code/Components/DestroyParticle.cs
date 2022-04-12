c4c0fa5cdc6cb42790f8493b0cb4f6d021b68294
Seventy <102102264+7Seventy0@users.noreply.github.com>
2022-04-12 13:51:22 +0200

    Source

---
+++ Source Code/Components/DestroyParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     float timeTillDeath = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        timeTillDeath -= Time.deltaTime;
+        if(timeTillDeath <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
